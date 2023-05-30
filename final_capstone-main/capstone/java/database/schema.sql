b1a39101813ea6b2e9773784350ad9c3f2e062dc
phillips-jd <jdphillips34@protonmail.com>
2023-05-30 15:04:24 -0400

    modified schema file to include username, password and role

--- final_capstone-main/capstone/java/database/schema.sql
+++ final_capstone-main/capstone/java/database/schema.sql
@@ -4,6 +4,9 @@ DROP TABLE IF EXISTS users, pets, adoption, pet_adoption;
 
 CREATE TABLE users (
 	user_id SERIAL,
+    username varchar(50) NOT NULL UNIQUE,
+    password_hash varchar(200) NOT NULL,
+    role varchar(50) NOT NULL,
 	first_name varchar(50) NOT NULL,
 	last_name varchar(50) NOT NULL,
 	email_address varchar(50) NOT NULL,
