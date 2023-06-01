3a059b93be0a827b4ddba923e15f889c331a6012
John Simpson <[A[Bjohn.a.simpson.ii@gmail.com>
2023-06-01 12:03:08 -0400

    Added Daos & Controllers 6/01/23

---
+++ final_capstone-main/capstone/java/src/main/java/com/techelevator/dao/AdoptionDao.java
@@ -0,0 +1,17 @@
+package com.techelevator.dao;
+
+import com.techelevator.model.Adoption;
+import com.techelevator.model.User;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface AdoptionDao {
+
+    List<Adoption> findAll();
+
+    Adoption getAdoptionById(int adoptionId);
+
+    boolean create(int petId, String ownerName, String ownerAddress, String ownerEmail, LocalDate adoptionDate);
+
+}
