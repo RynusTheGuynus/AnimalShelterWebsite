ccfbc4f525e9b473fecf881a10d31a8f16b8ca82
Riley Williamson <rwilliamson2053@gmail.com>
2023-06-05 12:27:28 -0600

    Search users by first name works

--- final_capstone-main/capstone/vue/src/views/Admin.vue
+++ final_capstone-main/capstone/vue/src/views/Admin.vue
@@ -3,6 +3,10 @@
       <shelter-header />
       <approve-pending/>
       <add-admin/>
+      <search-user :search-query="searchQuery" @search="handleSearch" />
+      <ul>
+        <li v-for="user in filteredUsers" :key="user.id">{{ user.first_name }}</li>
+      </ul>
       <shelter-footer/>
   </div>
 </template>
@@ -10,15 +14,33 @@
 <script>
 import AddAdmin from '../components/AddAdmin.vue'
 import ApprovePending from '../components/ApprovePending.vue'
+import SearchUser from '../components/SearchUser.vue'
 import ShelterFooter from '../components/ShelterFooter.vue'
 import ShelterHeader from '../components/ShelterHeader.vue'
 
 export default {
   name: 'Admin',
-  components: { ApprovePending, AddAdmin, ShelterHeader, ShelterFooter },
-
-}
+  components: { ApprovePending, AddAdmin, ShelterHeader, ShelterFooter, SearchUser },
+  data() {
+    return {
+      users: [],
+      searchQuery: '',
+    };
+  },
+  computed: {
+    filteredUsers() {
+      return this.users.filter(user => user.first_name.includes(this.searchQuery));
+  },
+  },
+  methods: {
+    handleSearch(query) {
+      console.log('Received search query:', query);
+      this.searchQuery = query;
+    },
+  },
+};
 </script>
+    
 
 <style>
 
