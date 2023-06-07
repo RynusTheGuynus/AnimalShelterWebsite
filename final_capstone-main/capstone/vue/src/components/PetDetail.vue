a4c839ded7c1e335a1336de7d76db3e049e83083
Adam Suver <asuver4@gmail.com>
2023-06-07 13:47:39 -0400

    Updates to Pet Detail

--- final_capstone-main/capstone/vue/src/components/PetDetail.vue
+++ final_capstone-main/capstone/vue/src/components/PetDetail.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="pet-detail-container">
-      <h2>{{this.pet.pet_name}}</h2>
+      <h1 id="nameHeader">{{this.pet.pet_name}}</h1>
       <div id="photoAndDetails">
       <div id="imageContainer">
       <img :src="images[this.currentIndex].image_path" alt="Available Pet Image" id="detailImage"> <br>
@@ -80,6 +80,7 @@ export default {
     display: flex;
     flex-direction: column;
     align-items: stretch;
+    background-image: linear-gradient(to right, tan, white)
 }
 
 #imageContainer {
@@ -90,17 +91,21 @@ export default {
 
 #photoAndDetails {
     display: flex;
-    margin-top: 75px;
+    margin-top: 50px;
     justify-content: space-evenly;
 }
 
 #petDescription {
     margin: auto;
+    margin-bottom: 50px;
     margin-top: 25px;
 }
 #detailImage {
   height: 240px;
   width: 360px;
   border-radius: 5%;
+}
+#nameHeader {
+    color: black;
 }
 </style>
