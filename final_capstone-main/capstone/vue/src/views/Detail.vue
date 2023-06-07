7baf1fa8e282f10516b7317e46baf28648448493
Adam Suver <asuver4@gmail.com>
2023-06-07 16:00:13 -0400

    Minor updates to Pet Detail

--- final_capstone-main/capstone/vue/src/views/Detail.vue
+++ final_capstone-main/capstone/vue/src/views/Detail.vue
@@ -1,18 +1,20 @@
 <template>
   <div>
       <shelter-header/>
+      <navigation-bar id="navigationBar"/>
       <pet-detail></pet-detail>
       <shelter-footer/>
   </div>
 </template>
 
 <script>
+import NavigationBar from '../components/NavigationBar.vue'
 import PetDetail from '../components/PetDetail.vue'
 import ShelterFooter from '../components/ShelterFooter.vue'
 import ShelterHeader from '../components/ShelterHeader.vue'
 export default {
     name: 'detail',
-    components: { PetDetail, ShelterHeader, ShelterFooter },
+    components: { PetDetail, ShelterHeader, ShelterFooter, NavigationBar },
 }
 </script>
     
