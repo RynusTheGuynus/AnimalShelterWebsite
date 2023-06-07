49c56889030813bec025bb57d97b7b908bc114e6
Riley Williamson <rwilliamson2053@gmail.com>
2023-06-07 11:13:00 -0600

    Nav bar is across the whole website and looks better

--- final_capstone-main/capstone/vue/src/components/NavigationBar.vue
+++ final_capstone-main/capstone/vue/src/components/NavigationBar.vue
@@ -1,20 +1,19 @@
 <template>
   <div>
-    <div id="NavBar">
+    <nav id="NavBar">
       <ul>
         <li><router-link :to="{ name: 'home' }" class="router-link">Home</router-link></li>
         <li><router-link :to="{ name: 'about' }" class="router-link">About Us</router-link></li>
-        <li><router-link :to="{ name: 'pets' }" class="router-link">Pets Available for Adoption</router-link></li>
+        <li><router-link :to="{ name: 'pets' }" class="router-link">Pets Available</router-link><br>for Adoption</li>
         <li><router-link :to="{ name: 'celebrate' }" class="router-link">Second Chances!</router-link></li>
-        <li><router-link :to="{ name: 'register' }" class="router-link">Apply to be a Volunteer!</router-link></li>
-        <!-- We need to add the admin view to the nav and base its visibility on user auth/role -->
-        <li v-if="$store.state.token !== ''"><router-link :to="{ name: 'admin' }" class="router-link">Admin</router-link></li>
-        <li v-if="$store.state.token !== ''"><router-link :to="{ name: 'update' }" class="router-link">Add/Update Pets</router-link></li>
-        <li v-if="$store.state.token !== ''"><router-link :to="{ name: 'users' }" class="router-link">Volunteer Directory</router-link></li>
+        <li><router-link :to="{ name: 'register' }" class="router-link">Apply to be a<br>Volunteer!</router-link></li>
+        <li v-if="$store.state.token !== ''" class="admin-link"><router-link :to="{ name: 'admin' }" class="router-link">Admin</router-link></li>
+        <li v-if="$store.state.token !== ''" class="admin-link"><router-link :to="{ name: 'update' }" class="router-link">Add/Update Pets</router-link></li>
+        <li v-if="$store.state.token !== ''" class="admin-link"><router-link :to="{ name: 'users' }" class="router-link">Volunteer Directory</router-link></li>
         <li v-if="$store.state.token === ''"><router-link :to="{ name: 'login' }" class="router-link">Volunteer Login</router-link></li>
-        <li v-if="$store.state.token !== ''"><router-link :to="{ name: 'logout' }" class="router-link">Volunteer Logout</router-link></li>
+        <li v-if="$store.state.token !== ''" class="router-link"><router-link :to="{ name: 'logout' }" class="router-link">Volunteer Logout</router-link></li>
       </ul>
-    </div>
+    </nav>
   </div>
 </template>
 
@@ -22,12 +21,15 @@
 export default {};
 </script>
 
-<style >
-
+<style>
 #NavBar {
   display: flex;
   justify-content: center;
   width: 100%;
+  background-color: rgb(92, 59, 10);
+  padding: 10px 0;
+  border-radius: 5px;
+  white-space: nowrap; /* Prevent line breaks within the navigation links */
 }
 
 ul {
@@ -35,11 +37,24 @@ ul {
   list-style: none;
   margin: 0;
   padding: 0;
+  justify-content: center; /* Center the navigation links */
+  flex-wrap: wrap; /* Allow wrapping of the navigation items */
 }
 
 li {
-  margin-right: 20px;
-  font-size: 16px;
-  color: antiquewhite;
+  font-size: 12px;
+  text-align: center;
+}
+
+.router-link {
+  text-decoration: none;
+  color: white;
+  padding: 5px 5px; /* Adjust the padding to reduce the spaces */
+  border-radius: 5px;
+  margin: 5px; /* Adjust the margin to reduce the spaces */
+}
+
+.router-link:hover {
+  background-color: rgb(85, 85, 85); /* Change the background color of the links on hover */
 }
 </style>
--- final_capstone-main/capstone/vue/src/views/Register.vue
+++ final_capstone-main/capstone/vue/src/views/Register.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <shelter-header />
+    <navigation-bar id='navigationBar' />
     <volunteer-form/>
     <shelter-footer/>
   </div>
@@ -10,11 +11,11 @@
 import ShelterFooter from '../components/ShelterFooter.vue'
 import VolunteerForm from '../components/VolunteerForm.vue'
 import ShelterHeader from '../components/ShelterHeader.vue'
-
+import NavigationBar from '../components/NavigationBar.vue'
 
 
 export default {
-  components: { VolunteerForm, ShelterHeader, ShelterFooter },
+  components: { VolunteerForm, ShelterHeader, ShelterFooter, NavigationBar },
   
   name: 'register'
 }
